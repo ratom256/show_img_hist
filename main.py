908ef93302b9bcac7587dae0be49cfa5f03c99fc
Shinjiro Okui <ratom256.learn@gamil.com>
2024-06-13 22:06:53 +0900

    chance caption text

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons,my CI/CD task',
+        caption='image from wikimedia commons,CI/CD my task',
         use_column_width=True
     )
 
